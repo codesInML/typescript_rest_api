a8e2010ec5dd9308e2aea9bdf45eceb6303cfd34
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2021-11-06 15:45:10 +0100

    completed the functionality to create and add users to the database

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ const PORT = config.get<number>("port")
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(PORT, async () => {
     log.info(`App is running on http://localhost:${PORT}`)
 
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,13 +1,15 @@
 import {Request, Response} from 'express'
+import { CreateUserInput } from '../schema/user.schema'
+import { omit } from 'lodash'
 import { createUser } from '../service/user.service'
 import log from '../utils/logger'
 
-export async function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(req: Request<{}, {}, CreateUserInput["body" ]>, res: Response) {
     try{
         const user = await createUser(req.body)
-        res.status(200).json({msg: "success", user})
+        res.status(200).json({msg: "success", user: omit(user.toJSON(), 'password')})
     } catch (err: any) {
-        log.error(err)
+        log.error(err.message)
         return res.status(409).send(err.message)
     }
 }
